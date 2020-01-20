4b445e97802d0404ddc98c17caefb432a1fc0c78
Samuel Lindberg <lindberg.samuel@gmail.com>
2020-01-20 16:27:16 +0100

    Added more Identity stuff and created the Identity DB

---
+++ BusinessTournaments/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessTournaments.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusinessTournaments.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        private readonly AccountService service;
+
+        public AccountController(AccountService service)
+        {
+            this.service = service;
+        }
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            return Content("Hello Business TOurmant!");
+        }
+    }
+}
---
+++ BusinessTournaments/Models/AccountService.cs
@@ -0,0 +1,24 @@
+﻿using BusinessTournaments.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessTournaments.Models
+{
+    public class AccountService
+    {
+        private readonly UserManager<CompanyUser> userManager;
+        private readonly SignInManager<CompanyUser> signInManager;
+
+        public AccountService(
+            UserManager<CompanyUser> userManager,
+            SignInManager<CompanyUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+    }
+}
