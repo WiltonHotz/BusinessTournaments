9f444d75b2696f7cbb53c8366dfa8d3a2e425f36
Samuel Lindberg <lindberg.samuel@gmail.com>
2020-01-20 15:48:56 +0100

    Added Identity config in startup + Identity classes.

---
+++ BusinessTournaments/Models/Identity/CompanyContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessTournaments.Models.Identity
+{
+    public class CompanyContext : IdentityDbContext<CompanyUser> 
+    {
+        public CompanyContext(DbContextOptions<CompanyContext> options) : base(options)
+        {
+            var result = Database.EnsureCreated();
+        }
+    }
+}
--- BusinessTournaments/Startup.cs
+++ BusinessTournaments/Startup.cs
@@ -2,9 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusinessTournaments.Models.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +16,27 @@ namespace BusinessTournaments
 {
     public class Startup
     {
+        private readonly IConfiguration config;
+
+        public Startup(IConfiguration config)
+        {
+            this.config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<CompanyContext>(c => c.UseSqlServer(connString));
+
+            services.AddIdentity<CompanyUser, IdentityRole>()
+                .AddEntityFrameworkStores<CompanyContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(c => c.LoginPath = "/login");
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +47,14 @@ namespace BusinessTournaments
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseAuthentication();
             app.UseRouting();
+            app.UseAuthorization();
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
