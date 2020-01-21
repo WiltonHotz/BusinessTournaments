61fbef59c02cca4f1695dd7d0d5a6ee839e939a2
Wilton Hotz <v.horwing@gmail.com>
2020-01-21 14:07:34 +0100

    Added ViewModels

---
+++ BusinessTournaments/Models/ViewModels/PlayerVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusinessTournaments.Models.Identity
+{
+    public class PlayerVM
+    {
+        public int PlayerId { get; set; }
+        public string PlayerName { get; set; }
+        public int? Score { get; set; }
+    }
+}
