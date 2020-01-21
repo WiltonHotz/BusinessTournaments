05102ce1fafdcaac7fcf36a0602c3a9de1745f13
Wilton Hotz <v.horwing@gmail.com>
2020-01-21 14:07:34 +0100

    Added ViewModels

---
+++ BusinessTournaments/Models/ViewModels/IndexVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusinessTournaments.Models.ViewModels
+{
+    public class IndexVM
+    {
+        public IEnumerable<PlayerVM> Leaderboard { get; set; }
+        public IEnumerable<TournamentVM> OngoingTournaments { get; set; }
+        public IEnumerable<TournamentVM> CompletedTournaments { get; set; }
+    }
+}
