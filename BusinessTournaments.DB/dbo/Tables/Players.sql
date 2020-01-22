8c2207c1b4b0bf653f18cdc3e966158bffccfb94
Wilton Hotz <v.horwing@gmail.com>
2020-01-22 13:58:31 +0100

    Fixed Validation Add new players

--- BusinessTournaments.DB/dbo/Tables/Players.sql
+++ BusinessTournaments.DB/dbo/Tables/Players.sql
@@ -3,8 +3,8 @@
     [Name]      NVARCHAR (30) NOT NULL,
     [CompanyID] Nvarchar(450)           NOT NULL,
     [Score]     INT           Not NULL,
+    UNIQUE ([Name], [CompanyID]),
     PRIMARY KEY CLUSTERED ([Id] ASC),
     FOREIGN KEY ([CompanyID]) REFERENCES [dbo].[AspNetUsers] ([Id]),
-    UNIQUE NONCLUSTERED ([Name] ASC)
 );
 
