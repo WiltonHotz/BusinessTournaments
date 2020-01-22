2e21b54ea428ac34636ca18f96518c8a98fad687
Wilton Hotz <v.horwing@gmail.com>
2020-01-22 17:00:27 +0100

    Added edit player structure

--- BusinessTournaments/wwwroot/js/index.js
+++ BusinessTournaments/wwwroot/js/index.js
@@ -17,7 +17,6 @@ function getIndexVMJSON() {
         url: url,
         type: "GET",
         success: function (response) {
-            console.log(response)
             PopulatePlayersOnLoad(response.leaderboard);
             PopulateOngoingTournamentsOnLoad(response.ongoingTournaments)
             PopulateCompletedTournamentsOnLoad(response.completedTournaments)
@@ -26,7 +25,6 @@ function getIndexVMJSON() {
 }
 
 function PopulatePlayersOnLoad(leaderboard) {
-    console.log(leaderboard)
 
     for (var i = 0; i < leaderboard.length; i++) {
         let arrow = `<span class="select-button" id="sBtn${leaderboard[i].playerId}" onclick="selectPlayer('${leaderboard[i].playerId}','${leaderboard[i].playerName}')"><svg viewBox="0 0 10 16" width="20" height="35" version="1.1" class="octicon octicon-arrow-right"><path fill-rule="evenodd" d="M10 8L4 3v3H0v4h4v3z"/></svg>`;
@@ -35,14 +33,13 @@ function PopulatePlayersOnLoad(leaderboard) {
             .append(`<tr style="height: 38px" id='l${leaderboard[i].playerId}'>
                         <td>${leaderboard[i].score}</td>
                         <td>${leaderboard[i].playerName}</td>
-                        <td><span class="editIcon" id="editPlayer${i}" onclick="editPlayer(${leaderboard[i].playerName})"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 14 16" fill="currentColor"><path fill-rule="evenodd" d="M0 12v3h3l8-8-3-3-8 8zm3 2H1v-2h1v1h1v1zm10.3-9.3L12 6 9 3l1.3-1.3a.996.996 0 0 1 1.41 0l1.59 1.59c.39.39.39 1.02 0 1.41z"/></svg></span></td>
+                        <td><span class="editIcon" id="editPlayer${i}" data-toggle="modal" data-target="#editPlayerModal" onclick="editPlayer('${leaderboard[i].playerName}','${leaderboard[i].playerId}')"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 14 16" fill="currentColor"><path fill-rule="evenodd" d="M0 12v3h3l8-8-3-3-8 8zm3 2H1v-2h1v1h1v1zm10.3-9.3L12 6 9 3l1.3-1.3a.996.996 0 0 1 1.41 0l1.59 1.59c.39.39.39 1.02 0 1.41z"/></svg></span></td>
                         <td style="width: 20px" id="selectarrowtd${leaderboard[i].playerId}">${arrow}</td>
                         </tr>`);
     }
 }
 
 function PopulateOngoingTournamentsOnLoad(tournaments) {
-    console.log(tournaments)
     for (var i = 0; i < tournaments.length; i++) {
         let deleteButton = `<span class="select-button" id="deleteBtn${tournaments[i].tournamentId}" onclick="deleteTournament('${tournaments[i].tournamentId}')"><svg class="octicon octicon-x" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48L7.48 8z"/></svg>`;
         $("#ongoing")
@@ -55,7 +52,7 @@ function PopulateOngoingTournamentsOnLoad(tournaments) {
 }
 
 function PopulateCompletedTournamentsOnLoad(tournaments) {
-    console.log(tournaments)
+
     for (var i = 0; i < tournaments.length; i++) {
         $("#completed")
             .append(`<tr id='ct${tournaments[i].tournamentId}'>
@@ -69,18 +66,45 @@ function ReturnDateFormat(date) {
     return date.substring(0, 10) + " " + date.substring(11, 16)
 }
 
+function editPlayer(playerName, playerId) {
+    console.log(playerName)
+    $("#editPlayerName").val(playerName);
+    console.log(editPlayerId)
+    $("#editPlayerId").val(playerId);
+}
+
+function confirmEditPlayer() {
+    $("#editPlayerId").val();
+
+    var txt;
+    var r = confirm(`Are you sure you want to change name to\n${$("#editPlayerName").val()}`);
+    if (r == true) {
+        txt = "You pressed OK!";
+    } else {
+        txt = "You pressed Cancel!";
+    }
+}
+function confirmDeletePlayer() {
+    $("#editPlayerId").val();
+    console.log("after delete pressed " + $("#editPlayerId").val())
+    var txt;
+    var r = confirm(`Are you sure you want to delete player\n${$("#editPlayerName").val()}`);
+    if (r == true) {
+        txt = "You pressed OK!";
+    } else {
+        txt = "You pressed Cancel!";
+    }
+}
+
 function addPlayers() {
 
     var inpIds = [];
     var names = [];
     var c = document.querySelectorAll("#modalPlayerNames > div");
-    console.log(c);
     for (var i = 0; i < c.length; i++) {
         inpIds[i] = `pninp${i}`
         names[i] = $(`#${inpIds[i]}`).val()
     }
-    console.log(inpIds)
-    console.log(names)
 
     $.ajax({
         url: 'AddPlayers',
