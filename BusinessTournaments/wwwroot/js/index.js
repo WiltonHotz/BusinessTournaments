7ef528b65bc12413f6357fd4d57b623cdfd5a59a
Catpuppie <anjaarnesson@gmail.com>
2020-01-24 16:12:09 +0100

    JavaSctipt BurgerStuff Method. And testing for new hamburger menu overlay

--- BusinessTournaments/wwwroot/js/index.js
+++ BusinessTournaments/wwwroot/js/index.js
@@ -713,5 +713,10 @@ $(document).ready(function () {
 });
 
 function burgerStuff() {
-
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
 }
