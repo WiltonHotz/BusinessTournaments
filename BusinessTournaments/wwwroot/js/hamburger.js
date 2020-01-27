28655b464f0eb29944adebec785e046593a287df
Samuel Lindberg <lindberg.samuel@gmail.com>
2020-01-27 16:59:24 +0100

    Fixed hamburger stuff.

---
+++ BusinessTournaments/wwwroot/js/hamburger.js
@@ -0,0 +1,9 @@
+﻿function openNav() {
+    var sidebar = document.getElementById("mySidenav");
+    sidebar.style.width = "200px";
+    //sidebar.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
--- BusinessTournaments/wwwroot/js/index.js
+++ BusinessTournaments/wwwroot/js/index.js
@@ -868,12 +868,3 @@ function setTheme(theme) {
 //#endregion
 
 
-function openNav() {
-    var sidebar = document.getElementById("mySidenav");
-    sidebar.style.width = "200px";
-    //sidebar.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
