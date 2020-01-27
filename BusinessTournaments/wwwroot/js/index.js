63d392ee34edd25bfd4b9fa6404591d71fd60126
Samuel Lindberg <lindberg.samuel@gmail.com>
2020-01-27 15:36:54 +0100

    Added samuelTheme with experimental looks.

--- BusinessTournaments/wwwroot/js/index.js
+++ BusinessTournaments/wwwroot/js/index.js
@@ -56,7 +56,7 @@ $(document).ready(function () {
 //#region Event listeners
 document.addEventListener("DOMContentLoaded", function (event) {
     getIndexVMJSON()
-    setTheme('default');
+    setTheme('samuelsTheme');
 });
 document.querySelectorAll(".tableFixHead").forEach(el =>
     el.addEventListener("scroll", tableFixHead)
@@ -87,8 +87,8 @@ function PopulatePlayersOnLoad(leaderboard) {
         $("#leaderboard")
             .append(`<tr style="height: 38px;" id='l${leaderboard[i].playerId}' class="leaderboard-row">
                         <td><span class="editIcon" id="editPlayer${leaderboard[i].playerId}" data-toggle="modal" data-target="#editPlayerModal" onclick="editPlayer('lname${leaderboard[i].playerId}','${leaderboard[i].playerId}')"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 14 16" fill="currentColor"><path fill-rule="evenodd" d="M0 12v3h3l8-8-3-3-8 8zm3 2H1v-2h1v1h1v1zm10.3-9.3L12 6 9 3l1.3-1.3a.996.996 0 0 1 1.41 0l1.59 1.59c.39.39.39 1.02 0 1.41z"/></svg></span></td>
-                        <td>${leaderboard[i].score}</td>
-                        <td id="lname${leaderboard[i].playerId}">${leaderboard[i].playerName}</td>
+                        <td class="scoretd">${leaderboard[i].score}</td>
+                        <td class="nametd" id="lname${leaderboard[i].playerId}">${leaderboard[i].playerName}</td>
                         ${canAddMorePlayers ? `<td style="width: 5px; text-align: right; padding-right: 15px;" id="selectarrowtd${leaderboard[i].playerId}">${arrow}</td>` : `<td style="width: 5px; text-align: right; visibility: hidden; padding-right: 15px;" id="selectarrowtd${leaderboard[i].playerId}">${arrow}</td>`}
                         </tr>`);
     }
